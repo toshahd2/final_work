fad44945a9a7826a73d31034776fe38bfb7b3e44
Антон Егоров <anton@192.168.1.9>
2022-09-17 16:57:55 +0300

    add program

---
+++ task/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Write("Введите количество элементов в массиве: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[size];
+FillArray(array);
+PrintArray(array);
+
+int newSize = 0;
+string[] newArray = new string[size];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length < 4)
+    {
+        newArray[newSize] = array[i];
+        newSize++;
+    }
+}
+
+Array.Resize(ref newArray, newSize);
+PrintArray(newArray);
+
+void FillArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("Введите значение элемента массива: ");
+        array[i] = Console.ReadLine();
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
